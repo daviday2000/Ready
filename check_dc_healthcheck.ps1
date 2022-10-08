c82723dc0e745daa47bec546bef53a2b72a2e497
daviday2000 <daviday2000@hotmail.com>
2022-10-08 11:16:46 -0400

    Create check_dc_healthcheck.ps1

---
+++ check_dc_healthcheck.ps1
@@ -0,0 +1,27 @@
+﻿function Invoke-DcDiag {
+    param(
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]$DomainController
+    )
+    $dcdiag_res = New-Object Object
+    $dcdiag_res | Add-Member -Type NoteProperty -Name "DC" -Value $DomainController
+    $result = dcdiag /s:$DomainController
+    $result_all = $result | select-string -pattern '\. (.*) \b(passed|failed)\b test (.*)' 
+    $result_all | foreach {
+        $dcdiag_res | Add-Member -Type NoteProperty -Name $($_.Matches.Groups[3].Value) -Value $($_.Matches.Groups[2].Value) -force
+    }
+    $dcdiag_res
+}
+
+
+$AllDCDiags = @()
+$DCs = ([System.DirectoryServices.ActiveDirectory.Forest]::GetCurrentForest()).sites
+foreach ($DC in $DCs.servers) {
+    Write-Host "Working on... $($DC.name)"
+    $dcdiag_res = Invoke-DcDiag -DomainController $DC.name
+    $AllDCDiags += $dcdiag_res
+}
+
+$AllDCDiags | Out-GridView
+$AllDCDiags | ft -AutoSize
